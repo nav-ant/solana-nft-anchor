21f36c463b2b6528cfd2c86b177f95c6593f4e5a
nav-ant <60328596+nav-ant@users.noreply.github.com>
2024-06-08 22:48:38 +0100

    Update program ID

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("EjttB2ZibQuwPvqbzFQApYfe6xzXy781fxMrVzSamu1C");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
